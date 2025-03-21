04555b038bf3e6fd20a6c3612a61b35efebfa3b4
Crinfarr <dev@crinfarr.io>
2025-03-21 02:52:53 -0500

    start over

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,53 +1,18 @@
-#define DEBUG
-
 #include <Arduino.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Encoder.h>
-#include <OneButton.h>
-#include <Wire.h>
-
-#ifdef DEBUG
-#include <Serial.h>
-#endif
-
-//Screen defines
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-#define SCREEN_ADDRESS 0x3D
-
-Encoder* UserInputController;
-long EncoderState = -999;
-OneButton* UserInputSelector;
-Adafruit_SSD1306* Screen;
-
+#include <U8g2lib.h>
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C display(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+int drawcolor = 1;
 void setup() {
-    #ifdef DEBUG
-    Serial.begin(9600);
-    #endif
-    UserInputController = new Encoder(2, 3);
-    UserInputSelector = new OneButton(3);
-    Screen = new Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-    if (!Screen->begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-        #ifdef DEBUG
-        Serial.println('FAILED TO INIT DISPLAY');
-        #endif
-        for(;;);
-    }
-    Screen->clearDisplay();
-    const int16_t cx = Screen->width()/2;
-    const int16_t cy = Screen->height()/2;
-    const int16_t rad = min<int16_t>(2*cx, 2*cy)/2;
-    const int16_t cstroke = 1;
-    Screen->drawCircle(cx, cy, rad, SSD1306_WHITE);
-    Screen->drawCircle(cx, cy, rad - cstroke, SSD1306_BLACK);
-    Screen->display();
+    display.begin();
+    display.clearBuffer();
+    display.sendBuffer();
 }
-
-void loop() {
-    volatile long nEncoderState = UserInputController->read();
-    if (nEncoderState != EncoderState)
-        EncoderState = nEncoderState;
-    
+void loop(){
+    display.setDrawColor(++drawcolor%2);
+    uint8_t nbox[4] = {random(0,128), random(0,64), random(0,128), random(0,64)};
+    uint8_t box[4] = {min(nbox[0], nbox[2]), min(nbox[1], nbox[3]), max(nbox[0], nbox[2]), max(nbox[1], nbox[3])};
+    display.drawBox(box[0], box[1], box[2], box[3]);
+    display.sendBuffer();
+    Serial.println('['+String(box[0], HEX)+','+String(box[1], HEX)+','+String(box[2], HEX)+','+String(box[3], HEX)+']');
+    delay(20);
 }
