48006167860954c423a3ba20ac03f159a7095189
Crinfarr <dev@crinfarr.io>
2025-03-22 00:01:00 -0500

    Dial that makes slices tick around

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,76 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <Encoder.h>
+#include <OneButton.h>
+
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C display(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 int drawcolor = 1;
+Encoder uinput(9, 8);
+OneButton uclick(10);
+
+const int maxAmmo = 10;
+int ammoCt = 10;
+unsigned long bounceDelta = millis();
+
 void setup() {
     display.begin();
     display.clearBuffer();
+    for (int i = 0; i <= maxAmmo; i++) {
+        if (i <= ammoCt) {
+            display.drawBox((i - 1) * 2 + i * 12, 0, 12, 64);
+        }
+        else {
+            display.drawFrame((i - 1) * 2 + i * 12, 0, 12, 64);
+        }
+    }
     display.sendBuffer();
 }
-void loop(){
-    display.setDrawColor(++drawcolor%2);
-    uint8_t nbox[4] = {random(0,128), random(0,64), random(0,128), random(0,64)};
-    uint8_t box[4] = {min(nbox[0], nbox[2]), min(nbox[1], nbox[3]), max(nbox[0], nbox[2]), max(nbox[1], nbox[3])};
-    display.drawBox(box[0], box[1], box[2], box[3]);
+
+void loop() {
+    delay(200);
+    int newpos = uinput.readAndReset();
+    int dir = (0 < newpos) - (newpos < 0);
+    if (dir == 0) return;
+    bounceDelta = millis();
+    if (dir < 0) {
+        Serial.println("Moved left");
+        ammoCt--;
+    }
+
+    if (dir > 0) {
+        Serial.println("Moved right");
+        ammoCt++;
+    }
+    display.clearBuffer();
+    for (int i = 0; i <= maxAmmo; i++) {
+        if (i <= ammoCt) {
+            display.drawBox((i - 1) * 2 + i * 12, 0, 12, 64);
+        }
+        else {
+            display.drawFrame((i - 1) * 2 + i * 12, 0, 12, 64);
+        }
+    }
     display.sendBuffer();
-    Serial.println('['+String(box[0], HEX)+','+String(box[1], HEX)+','+String(box[2], HEX)+','+String(box[3], HEX)+']');
-    delay(20);
+
+    // bool debounce = (bounceDelta + 10ul) < millis();
+    // if (newpos != encPos && !debounce) {
+    //     if (encPos < newpos) ammoCt++;
+    //     if (encPos > newpos) ammoCt--;
+    //     encPos = newpos;
+    //     Serial.println("Encoder changed to " + String(encPos, DEC));
+    //     display.clearBuffer();
+    //     for (int i = 0; i <= maxAmmo; i++) {
+    //         if (i <= ammoCt) {
+    //             Serial.print('O');
+    //             display.drawBox((i - 1) * 2 + i * 12, 0, 12, 64);
+    //         }
+    //         else {
+    //             Serial.print('X');
+    //             display.drawFrame((i - 1) * 2 + i * 12, 0, 12, 64);
+    //         }
+    //     }
+    //     Serial.println();
+    //     display.sendBuffer();
+    // }
+    // else if (!debounce) Serial.println("Debounce triggered");
 }
